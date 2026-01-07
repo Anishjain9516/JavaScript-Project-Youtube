845ac37f68536db0018f6946c1ca8217884c2e0f
Anish Jain <69389512+Anishjain9516@users.noreply.github.com>
2026-01-07 18:46:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+
+let isbooleantype= true
+let isInttype = 3
+let isStringtype = "Hehe" 
+
+console.log(typeof(isInttype));
+console.log(typeof(isStringtype));
+console.log(typeof(isbooleantype));
+console.log(typeof(null));
+console.log(typeof(undefined));
+// null is a datatype which have 0 as value
+// undefined is a datatype which is not defined
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
 console.log("hi")
+const accountid=1235
+let accountemail="haha@gmail.com"
+var accountpassword ="2131"
+accountcity= "Jaipur"
+let accountstate
+
+// accountid=121111111
+accountemail="hehehe@gmail.com"
+accountpassword="9090"
+accountcity="Delhi"
+
+// console.log(accountid)
+/* prefer not to use var datatype
+because of the issue in block scope and functional scope
+
+console.table([accountid,accountemail, accountpassword,accountcity,accountstate]);
